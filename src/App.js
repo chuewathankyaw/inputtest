ed2440d2545aa1e1bd9887ab7f54a3573b946a14
Mg Thung <thunghtike7@gmail.com>
2022-11-23 16:50:18 +0630

    new

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Forminput from './component/input';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Forminput from "./component/input";
+import Datashow from "./component/Datashow";
+import { Grid } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={<Forminput/>}
-          
-        />
-        </Routes>
+      <Grid container my={2} sx={{ margin: "2rem" }}>
+        <Grid sm={12} md={6}>
+          <Forminput />
+        </Grid>
+        <Grid sm={12} md={6}>
+          <Datashow />
+        </Grid>
+      </Grid>
     </div>
   );
 }
