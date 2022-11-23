645b860dbf39468e91c0a4e451e457c9b69416a4
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-11-23 16:49:13 +0630

    chue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import './App.css';
-import Forminput from './component/input';
+import { Forminput } from './component/input';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path="/"
-          element={<Forminput/>}
-          
-        />
-        </Routes>
+      <Forminput/>
     </div>
   );
 }
--- src/component/input.jsx
+++ src/component/input.jsx
@@ -1,8 +1,61 @@
-export default function Forminput() {
+
+import { Box, Container, Grid, InputAdornment, Stack, TextField } from "@mui/material";
+import { useState } from "react";
+import "./input.css"
+
+
+export const Forminput = () => {
+      const [value, setvalue] = useState("");
 
     return (
-        <div>
-            hello world
+      <Box marginTop={5}>
+        <div className="imput">
+          <Container >
+            <Grid container direction="row" spacing={4}>
+              <Grid item xl={12}>
+                <TextField
+                  label="id"
+                  required
+                  value={value}
+                  onChange={(e) => setvalue(e.target.value)}
+                  helperText={
+                    !value
+                      ? "Required"
+                      : "Do not share your password with anyone"
+                  }
+                />
+              </Grid>
+              <Grid item xl={12}>
+                <TextField
+                  label="Title"
+                  required
+                  value={value}
+                  onChange={(e) => setvalue(e.target.value)}
+                  helperText={
+                    !value
+                      ? "Required"
+                      : "Do not share your password with anyone"
+                  }
+                />
+              </Grid>
+              <Grid item xl={12}>
+                <TextField
+                  label="Desc"
+                  required
+                  value={value}
+                  onChange={(e) => setvalue(e.target.value)}
+                  //   error={!value}
+                  helperText={
+                    !value
+                      ? "Required"
+                      : "Do not share your password with anyone"
+                  }
+                />
+              </Grid>
+            </Grid>
+          </Container>
+          <Stack direction="row" spacing={2}></Stack>
         </div>
-    )
-}
+      </Box>
+    );
+};
